91ca9209ddcbec468b590e18d51d681527e4f880
Sarthak <sarthakshah11@gmail.com>
2019-09-06 23:24:55 +0530

    Delete Task 3 - Longest length of words.py

--- Task 3 - Longest length of words.py
+++
@@ -1,6 +0,0 @@
-w = input('Enter a list of things seperated by comma we will give u the maximum number of characters in longest number \n');
-lst=w.split(',');
-j=[];
-for i in lst:
-	j.append(len(i));
-print(max(j));

bd0c8a50b09a127ef95992da819c6190f8b0880f
Sarthak <sarthakshah11@gmail.com>
2019-09-07 01:53:20 +0530

    Update Task 4 - UpperLowercase.py

--- Task 4 - UpperLowercase.py
+++ Task 4 - UpperLowercase.py
@@ -1,4 +1,4 @@
 w = input('Enter Any Text that will come back in upper and lower case \n');
 
 print('Upper Case : {}'.format(w.upper()));
-print('Upper Case : {}'.format(w.lower()));
+print('Lower Case : {}'.format(w.lower()));

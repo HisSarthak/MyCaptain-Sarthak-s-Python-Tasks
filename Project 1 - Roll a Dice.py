a960e2be0063fcb15688bc77d3d902a1c5e7ba8a
Sarthak <sarthakshah11@gmail.com>
2019-09-15 16:46:05 +0530

    Project 1 - Roll a Dice
    simple script to roll a dice(get number between 1 - 6)

---
+++ Project 1 - Roll a Dice.py
@@ -0,0 +1,21 @@
+import random
+
+num = random.randint(1,6);
+
+if num == 1:
+    print(' • ');
+    
+elif num == 2:
+    print('• •');
+
+elif num == 3:
+    print('• • \n •');
+
+elif num == 4:
+    print('• •\n• •');
+
+elif num == 5:
+    print('• •\n • \n• •');
+
+else:
+    print('• •\n• •\n• •');

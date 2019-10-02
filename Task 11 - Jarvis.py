9b23e7893845514659698342508e9d72cb0368d5
Sarthak <sarthakshah11@gmail.com>
2019-10-02 12:54:15 +0530

    Added Internet check : still needs lots of updates

--- Task 11 - Jarvis.py
+++ Task 11 - Jarvis.py
@@ -3,7 +3,8 @@ import speech_recognition as sr
 import datetime
 import wikipedia
 import webbrowser
-from selenium import webdriver
+import urllib
+from urllib.request import urlopen
 
 engine = pyttsx3.init('sapi5');
 voices = engine.getProperty('voices');
@@ -29,7 +30,7 @@ def wishMe():
     else:
         speak("Good Evening Sir");
 
-    speak("I am Kentil, How can I help you??")
+    speak("I am Trophin, How can I help you??")
 
 
 def takeCommand():
@@ -48,7 +49,7 @@ def takeCommand():
     except Exception as e:
         print("ERROR:",e);
 
-        print("Say that again please...");
+        speak("Say that again please...");
         return "None";
     return query;
 
@@ -56,54 +57,92 @@ def OpenInChrome(url):
     chromedir = "C:\Program Files\Google\Chrome\Application\chrome.exe";
     webbrowser.Chrome(name=chromedir).open(url,2);
 
+def is_internet():
+    try:
+        urlopen('https://www.google.com', timeout=1);
+        return True;
+    except urllib.error.URLError as e:
+        return False;
+
+def search(str):
+    if 'for' in str:
+        subquery=str.split(' for ');
+        str1=subquery[1];
+    elif 'search' in str:
+        subquery=str.split(' search ');
+        str1=subquery[1];
+    else:
+        subquery=str.split(' ');
+        str1=' '.join(subquery[1:]);
+    link=prelink + str1;
+    OpenInChrome(link);
+
 if __name__ == "__main__":
-    wishMe();
-    
-    while True:
-        query = takeCommand().lower();
-    
-        """ Logic for executin tasks based on query """
-        if 'wikipedia' in query:
-            speak('Searching wikipedia...');
-            query = query.replace("wikipedia", "");
-            results = wikipedia.summary(query, sentences=2);
-            speak("According to Wikipedia");
-            speak(results);
+    if is_internet():
 
-        elif 'open youtube' in query:
-            speak('Opening Youtube...');
-            OpenInChrome("youtube.com");
-
-        elif 'open google' in query:
-            speak('Opening Google...');
-            OpenInChrome("google.com");
-
-        #elif 'open spotify' in query:
-            #speak('Opening Spotify...');
-            #OpenInChrome("https://accounts.spotify.com/en/login?continue=https:%2F%2Fopen.spotify.com%2Fbrowse%2Ffeatured");
-            
-        elif 'open beautiful audio editor' in query:
-            speak('Opening Beautiful Audio Editor');
-            OpenInChrome("https://beautifulaudioeditor.appspot.com/app");
-        elif 'open audio editor online' in query:
-            speak('Opening Beautiful Audio Editor');
-            OpenInChrome("https://beautifulaudioeditor.appspot.com/app");
-        elif 'open online audio editor' in query:
-            speak('Opening Beautiful Audio Editor');
-            OpenInChrome("https://beautifulaudioeditor.appspot.com/app");
-
-        elif 'github' in query:
-            speak('Opening Github');
-            OpenInChrome("https://github.com/HisSarthak?tab=repositories");
+        wishMe();
         
-        elif 'open cloud convert' in query:
-            speak('Opening Cloud Convert');
-            OpenInChrome("cloudconvert.com");
+        while True:
+            query = takeCommand().lower();
+        
+            """ Logic for executin tasks based on query """
+            if 'wikipedia' in query:
+                speak('Searching wikipedia...');
+                query = query.replace("wikipedia", "");
+                results = wikipedia.summary(query, sentences=2);
+                speak("According to Wikipedia");
+                speak(results);
 
-        elif 'open gmail' in query:
-            speak('Opening Gmail');
-            OpenInChrome("https://mail.google.com/mail/u/0/#inbox");
+            elif 'youtube' in query:
+                prelink="https://www.youtube.com/results?search_query=";
+                search(query);
+                speak('Opening Youtube...');
+            
+            elif 'search' in query:
+                if 'google' in query:
+                    prelink="https://www.google.com/search?q=";
+                    search(query);
+                elif 'youtube' in query:
+                    prelink="https://www.youtube.com/results?search_query=";
+                    search(query);
+                else:
+                    speak("Sorry Sir. Some error has occured");
+                    speak("Please try again.");
 
-        elif 'exit' in query:
-            speak('Ok Bye Sir');
-            exit();
+            elif 'Google' in query:
+                prelink="https://www.google.com/search?q=";
+                search(query);
+                speak('Opening Google...');
+
+            #elif 'open spotify' in query:
+                #speak('Opening Spotify...');
+                #OpenInChrome("https://accounts.spotify.com/en/login?continue=https:%2F%2Fopen.spotify.com%2Fbrowse%2Ffeatured");
+                
+            elif 'open beautiful audio editor' in query:
+                speak('Opening Beautiful Audio Editor');
+                OpenInChrome("https://beautifulaudioeditor.appspot.com/app");
+            elif 'open audio editor online' in query:
+                speak('Opening Beautiful Audio Editor');
+                OpenInChrome("https://beautifulaudioeditor.appspot.com/app");
+            elif 'open online audio editor' in query:
+                speak('Opening Beautiful Audio Editor');
+                OpenInChrome("https://beautifulaudioeditor.appspot.com/app");
+
+            elif 'github' in query:
+                speak('Opening Github');
+                OpenInChrome("https://github.com/HisSarthak?tab=repositories");
+            
+            elif 'open cloud convert' in query:
+                speak('Opening Cloud Convert');
+                OpenInChrome("cloudconvert.com");
+
+            elif 'open gmail' in query:
+                speak('Opening Gmail');
+                OpenInChrome("https://mail.google.com/mail/u/0/#inbox");
+
+            elif 'stop listening' in query:
+                speak('Ok Sir, I will be ready for further commands');
+                break;
+    else:
+        speak("Sorry sir, seems like you are disconnected.\nPlease check Your connection and try again");
+        exit();
